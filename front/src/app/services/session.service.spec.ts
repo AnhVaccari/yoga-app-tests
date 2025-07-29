70a7c61d2e620718daa5b8ef836bc159d2fa27f0
AnhVaccari <anh.vaccari@orange.fr>
2025-07-29 11:27:03 +0200

    test : session.service complete

--- front/src/app/services/session.service.spec.ts
+++ front/src/app/services/session.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { SessionService } from './session.service';
+import { SessionInformation } from '../interfaces/sessionInformation.interface';
 
 describe('SessionService', () => {
   let service: SessionService;
@@ -13,5 +14,47 @@ describe('SessionService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return observable of logged state', () => {
+    service.$isLogged().subscribe((isLogged) => {
+      expect(isLogged).toBeFalsy();
+    });
+  });
+
+  it('should log in user', () => {
+    const mockUser: SessionInformation = {
+      token: 'abcd',
+      type: 'Bearer',
+      id: 1,
+      username: 'test@test.com',
+      firstName: 'John',
+      lastName: 'Doe',
+      admin: false,
+    };
+
+    service.logIn(mockUser);
+
+    expect(service.isLogged).toBeTruthy();
+    expect(service.sessionInformation).toEqual(mockUser);
+  });
+
+  it('should log out user', () => {
+    const mockUser: SessionInformation = {
+      token: 'abcd',
+      type: 'Bearer',
+      id: 1,
+      username: 'test@test.com',
+      firstName: 'John',
+      lastName: 'Doe',
+      admin: false,
+    };
+
+    service.logIn(mockUser);
+
+    service.logOut();
+
+    expect(service.isLogged).toBeFalsy();
+    expect(service.sessionInformation).toBeUndefined();
   });
 });
